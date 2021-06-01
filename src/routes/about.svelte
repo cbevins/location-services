07e8e8af06e53dea768bf12ebb7f8f924d10efdb
cbevins <cbevins@montana.com>
2021-06-01 17:04:10 -0600

    Initial commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<h1>About this site</h1>
+
+<p>
+This is one of my 'tinker' sites where I sporadically work on various projects
+relating to fire behavior modeling, Javascipt, and web site development.
+For a complete listing of these sites, please visit my
+<a href="https://cbevins-home.netlify.app">home</a> page.
+</p>
---
+++ src/server.js
@@ -0,0 +1,20 @@
+import sirv from 'sirv';
+import polka from 'polka';
+import compression from 'compression';
+import * as sapper from '@sapper/server';
+
+const { PORT, NODE_ENV } = process.env;
+const dev = NODE_ENV === 'development';
+
+// Should we use Express instead?
+console.log ('\n\nserver.js re-started at: ', Date.now())
+
+polka() // You can also use Express
+	.use(
+		compression({ threshold: 0 }),
+		sirv('static', { dev }),
+		sapper.middleware()
+	)
+	.listen(PORT, err => {
+		if (err) console.log('error', err);
+	});
